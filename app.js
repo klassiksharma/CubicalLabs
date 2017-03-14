5db3f7d2f4f4aee1754c952377e905cecc44cc6e
klassiksharma <abksharma3@gmail.com>
2017-03-14 12:00:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var cubicalApp = angular.module("Cubical", []);
+
+cubicalApp.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }]);
+
+cubicalApp.all('/*', function (request, response, next) {
+        response.header("Access-Control-Allow-Origin", "*");
+        response.header("Access-Control-Allow-Headers", "X-Requested-With");
+        response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
+        next();
+    });
